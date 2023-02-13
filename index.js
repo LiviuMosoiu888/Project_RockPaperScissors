a6e4a121586b16b1f966993cd82b60d9ea23452f
Liviu Mosoiu <liviu.mosoiu@yahoo.com>
2023-02-13 21:30:53 +0000

    creating function getComputerChoice to return random option between rock, paper and scissors

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world!")
+function getComputerChoice() {
+    const threeOptionsList = ["Rock", "Paper", "Scissors"];
+    const randomPick = Math.floor(Math.random() * threeOptionsList.length);
+
+    console.log(threeOptionsList[randomPick]);
+}
+
+getComputerChoice();
